16092a8500a5e1e5d3cd1fa413580798d389ae99
oumaystore-hub <oumaystore@gmail.com>
2026-06-14 19:37:05 +0100

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import { defineConfig } from 'astro/config';
 export default defineConfig({
   site: 'https://oumaystore-hub.github.io',
   base: '/Th-tech/',
-  trailingSlash: 'always',
+  trailingSlash: 'ignore', // ⬅️ هذا هو التعديل الجوهري
   output: 'static',
 });
