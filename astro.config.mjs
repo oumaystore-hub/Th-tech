33efd4a8d24632241e7dfd1c6842fbb08bec0be3
oumaystore-hub <oumaystore@gmail.com>
2026-06-17 05:59:24 +0000

    fix: add base path to astro config to fix 404 links

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   site: 'https://oumaystore-hub.github.io',
-  base: '/Th-tech/',
+  base: '/Th-tech/', // هذا السطر هو الحل! يربط الروابط بمجلد المشروع
   trailingSlash: 'always',
   output: 'static',
 });
