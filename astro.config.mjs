3590ec1755ff48f194525ab09ef454b57b0afd4a
oumaystore-hub <oumaystore@gmail.com>
2026-09-04 17:29:29 +0100

    Modify site URL and base path in astro.config
    Updated site URL and added base path for sitemap integration.

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,14 @@ import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://oumaystore-hub.github.io/Th-tech',
-  integrations: [sitemap()],
+  site: 'https://oumaystore-hub.github.io',
+  base: '/Th-tech',
+
   output: 'static',
+
+  integrations: [
+    sitemap({
+      filter: (page) => page.startsWith('https://oumaystore-hub.github.io/Th-tech/'),
+    }),
+  ],
 });
