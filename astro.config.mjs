5e17d95d1caaf7fb264e063a7cb42632fc6a70d7
oumaystore-hub <oumaystore@gmail.com>
2026-09-04 17:06:02 +0100

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,8 @@
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 
-// https://astro.build/config
 export default defineConfig({
-  site: 'https://oumaystore-hub.github.io',
-  base: '/Th-tech',
+  site: 'https://oumaystore-hub.github.io/Th-tech',
   integrations: [sitemap()],
   output: 'static',
 });
