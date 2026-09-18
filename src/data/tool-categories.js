f374a9414dcbc4315c348544398fb1d52d538a0e
oumaystore-hub <oumaystore@gmail.com>
2026-09-18 11:31:05 +0100

    Add toolCategories for various tool types

---
+++ src/data/tool-categories.js
@@ -0,0 +1,37 @@
+export const toolCategories = {
+  json: [
+    'json-formatter',
+    'json-validator'
+  ],
+
+  text: [
+    'word-counter',
+    'line-counter',
+    'character-counter'
+  ],
+
+  code: [
+    'html-formatter',
+    'css-formatter',
+    'javascript-formatter',
+    'markdown-formatter'
+  ],
+
+  security: [
+    'jwt-generator',
+    'jwt-decoder',
+    'hash-generator',
+    'base64-encoder'
+  ],
+
+  color: [
+    'color-picker',
+    'hex-to-rgb',
+    'rgb-to-hex'
+  ],
+
+  image: [
+    'qr-generator',
+    'barcode-generator'
+  ]
+};
